58858f36ef46abb7f99c847b8ac0b6c0f0a19de3
genyleong <leongenesis0@gmail.com>
2016-04-30 09:51:23 -0500

    layout

---
+++ prueba.js
@@ -0,0 +1,17 @@
+var express = require('express');
+
+var app = express();
+
+app.set('views', __dirname +'/views');
+
+app.set('view engine', 'jade');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  var pregunta = [{titulo:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, nobis.", tags:"electronica", respuestas: "2", fecha: "asked 28 secs ago"}]
+
+    res.render('layout', { lista : pregunta});
+  })
+
+app.listen(3000);
