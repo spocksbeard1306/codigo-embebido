fe289ebb39260716f7f50448759fafe8e1fd272e
genyleong <leongenesis0@gmail.com>
2016-04-30 09:59:31 -0500

    final

--- prueba.js
+++ prueba.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.get('/', function(req, res){
   var pregunta = [{titulo:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, nobis.", tags:"electronica", respuestas: "2", fecha: "asked 28 secs ago"}]
 
-    res.render('layout', { lista : pregunta});
+    res.render('problemas', { lista : pregunta});
   })
 
 app.listen(3000);
