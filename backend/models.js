40773766f95017cdde5cc549f207cc88b8eae409
Elvis Torres Pérez <elvistorres0306@gmail.com>
2016-05-05 06:55:08 -0500

    Uups, olvide grabar esto
    cambios al init.js ahora si funca

--- backend/models.js
+++ backend/models.js
@@ -1,12 +1,14 @@
 var mongoose = require('mongoose');
 var userSchema = require('./schemas/user-sc.js');
 var commentSchema = require('./schemas/comment-sc.js');
+var AnsCommentSchema = require('./schemas/ans-comment-sc.js');
 var answerSchema = require('./schemas/answer-sc.js');
 var questSchema = require('./schemas/quest-sc.js');
 
 module.exports = {
     User: mongoose.model('User', userSchema),
     Comment: mongoose.model('Comment', commentSchema),
+    AnsComment: mongoose.model('Comment',AnsCommentSchema),
     Answer: mongoose.model('Answer', answerSchema),
     Quest: mongoose.model('Quest', questSchema)
 };
