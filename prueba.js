c6a0a9a7441e4068418e8c99dfc9f479dfe420f9
Paty Arakawa <paty.arakawa@gmail.com>
2016-07-20 01:37:19 -0500

    renderizando vistas

--- prueba.js
+++ prueba.js
@@ -6,11 +6,25 @@ app.set('views', __dirname +'/views');
 app.set('view engine', 'jade');
 
 app.use(express.static('public'));
-app.get('/', function(req, res){
-    var pregunta = [
+
+
+app.get("/",function(req,res){
+	var pregunta = [
         {titulo:"Ayuda en javascript, AJAX no devuelve data.", tags:"javascript, aax", respuestas: "2", fecha: "hace 2 horas"},
         {titulo:"Renderizar javascript en Jade con Express JS.", tags:"nodejs, express, jade", respuestas: "1", fecha: "ayer"}
     ];
-    res.render('problemas', { lista : pregunta});
-})
+	res.render('index', { lista : pregunta});
+});
+
+app.get("/new",function(req,res){
+	res.render('new');
+});
+
+app.get("/question",function(req,res){
+	res.render('question');
+});
+
+app.get("/error",function(req,res){
+	res.render('error');
+});
 app.listen(4000);
